a5ef5e45a50100cc1cd2e37e855cb25c77534ba8
Josecj11 <josecallejimenez11@gmail.com>
2025-02-26 19:19:13 +0100

    update

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cool = require("cool-ascii-faces");
+const app = express();
+const PORT = process.env.PORT || 16078;
+
+app.use(express.static("public"));
+
+app.get("/",(request,response)=>{
+    response.send("Hello from the server!");
+});
+
+app.get("/cool",(request,response)=>{
+    response.send(cool());
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}!`);
+});
